24ab50814f1d915981b3b5531ff11c828a4b8d6c
Gonzalo <godzsirma@gmail.com>
2020-02-11 10:20:46 +0100

    Primeros componentes creados.

--- proyectoProfesores/src/app/app.module.ts
+++ proyectoProfesores/src/app/app.module.ts
@@ -3,10 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CabeceraComponent } from './cabecera/cabecera.component';
+import { SidemenuComponent } from './sidemenu/sidemenu.component';
+import { FooterComponent } from './footer/footer.component';
+import { IndividualComponent } from './individual/individual.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CabeceraComponent,
+    SidemenuComponent,
+    FooterComponent,
+    IndividualComponent
   ],
   imports: [
     BrowserModule,
