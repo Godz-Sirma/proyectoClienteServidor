9ec69b656975ad46df25fa747857c0a49955e55e
Gonzalo <godzsirma@gmail.com>
2020-02-27 04:06:15 +0100

    Crud de las entidades generado

---
+++ symfony/proyectoSymfony/src/AppBundle/Form/UsuariosType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UsuariosType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('asnombre')->add('asapellido')->add('asdireccion')->add('aiedad')->add('asrol');
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Usuarios'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_usuarios';
+    }
+
+
+}
